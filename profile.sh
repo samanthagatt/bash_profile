3f8a970f6cb7db19dd5482d5d127df20193817df
Samantha Gatt <39925211+samanthagatt@users.noreply.github.com>
2020-02-11 15:17:13 -0500

    Uses custom function as default mkdir

--- profile.sh
+++ profile.sh
@@ -3,7 +3,7 @@ export PS1="\n\d, \t\n\w\n> "
 alias rm="rm -vr"
 
 # macOS uses BSD based code and supresses -v functionality when using -p
-mkdir-pv() {
+mkdir() {
     for arg in $@; do
         dir=""
         IFS="/"
@@ -11,19 +11,19 @@ mkdir-pv() {
         for i in "${ADDR[@]}"; do
             dir+="$i/"
             if [ ! -d "$dir" ]; then
-                mkdir -v "$dir"
+                command mkdir -v "$dir"
             fi
         done
         IFS=" "
     done
 }
 
-mkcd() { mkdir-pv "$1" && cd "$1"; }
+mkcd() { mkdir "$1" && cd "$1"; }
 
 cra() { create-react-app "$1" && cd "$1"; }
 
 mkcmpnt() {
-    mkdir-pv "$1"
+    mkdir "$1"
     touch "$1/$1.js" && echo "Created new file $1.js"
     touch "$1/$1.css" && echo "Created new file $1.css"
 }
